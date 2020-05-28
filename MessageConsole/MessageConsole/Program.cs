afb8e6f2919d01a63df41a98664a052ebb0a5ca1
karan.rana <karan.rana@IFOUR-11>
2020-05-28 14:45:37 +0530

    Signal R Add

---
+++ MessageConsole/MessageConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace MessageConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:59093/message";
+            var serverConnect = new HubConnectionBuilder()
+                 .WithUrl(new Uri(url)).WithAutomaticReconnect().Build();
+            serverConnect.StartAsync().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("Not Connected");
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+
+                    serverConnect.On<string>("BroadcastMessage", (message) =>
+                    {
+                        Console.WriteLine(message);
+                    });
+
+                    Console.ReadKey();
+                }
+            }).Wait();
+        }
+    }
+}
